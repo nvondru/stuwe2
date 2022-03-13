2082723335686d589c898776f2fc8497a2bc7a25
Nicolas Vondru <nvondru@macbook-pro-von-nicolas.home>
2022-03-13 22:52:40 +0100

    added Viewer WebGL build

--- Babuschka_PartyRun/Public/Controller/script.js
+++ Babuschka_PartyRun/Public/Controller/script.js
@@ -21,10 +21,3 @@ btnShoot.addEventListener("touchend", () => {
   console.log("released shoot btn");
   socket.emit("shoot end");
 });
-
-$(".no-zoom").bind("touchend", function (e) {
-  e.preventDefault();
-  // Add your code here.
-  $(this).click();
-  // This line still calls the standard click event, in case the user needs to interact with the element that is being clicked on, but still avoids zooming in cases of double clicking.
-});
